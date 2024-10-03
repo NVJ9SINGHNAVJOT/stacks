946554a7a9e1e0e8e8a13ebabb76e5e7c6267d47
NVJ9SINGHNAVJOT <nvjsinghnavjot4411@gmail.com>
2024-10-03 21:15:42 +0530

    fix(check network): :bug: function added

--- task_scripts/docker_proxy_network.sh
+++ task_scripts/docker_proxy_network.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Function to check if a network exists
+check_network_exists() {
+  network_name=$1
+  if docker network ls --format "{{.Name}}" | grep -w "$network_name" > /dev/null 2>&1; then
+    return 0  # Network exists
+  else
+    return 1  # Network does not exist
+  fi
+}
+
 # Create the proxy network (external)
 create_proxy_network() {
   if check_network_exists "proxy"; then
